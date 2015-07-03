80bfb7466c01f9ae269b536894b1cc691e4ae54a
venkataramana R <venkataramana.r88@gmail.com>
2015-07-03 14:30:25 +0530

    Integration with angular and login page setup done

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,8 +1,32 @@
-var phonecatApp = angular.module('phonecatApp', []);
-phonecatApp.controller('PhoneListCtrl', function ($scope) {
+var loginApp = angular.module('loginApp', ["ngRoute","loginControllers", "loginServices"]);
+
+loginApp.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/option1', {
+        templateUrl: 'login/partials/option1',
+        controller: 'option1Cntrl'
+      }).
+      when('/option2', {
+        templateUrl: 'login/partials/option2',
+        controller: 'option2Cntrl'
+      }).
+      otherwise({
+        redirectTo: '/option1'
+      });
+  }]);
+
+
+var loginControllers = angular.module('loginControllers', []);
+loginControllers.controller('MainCtrl',["$scope","$http",function ($scope,$http) {
    	$scope.user = {firstname:"",lastname:"",phoneno:"",gender:"",email:"",password:"",confirmpassword:""};
 
    	$scope.gender = [{name:"male"},{name:"female"}]
+
+   	$scope.toggleSideBar = function(){
+   		jQuery("#side_menu_container").toggleClass("hide");
+   		jQuery("#body_container").toggleClass("side-menu-included"); 
+   	}
 
    	$scope.signInDialog = function(user){
    		jQuery("#signup_container").removeClass("show");
@@ -23,4 +47,28 @@ phonecatApp.controller('PhoneListCtrl', function ($scope) {
    	$scope.usersignup = function(user){
    		console.log("signup",user);
    	}
-});
+}]);
+
+loginControllers.controller('option1Cntrl',["$scope","OptionService",function ($scope,OptionService) {
+	$scope.option = OptionService.query();
+}]);
+
+loginControllers.controller('option2Cntrl',["$scope","OptionService",function ($scope,OptionService) {
+	//$scope.option = OptionService.query();
+	OptionService.get({optionid: '2'}, function(optionvalue) {
+		console.log(optionvalue);
+    	$scope.option = optionvalue;
+  	});
+
+}]);
+
+
+
+var loginServices = angular.module('loginServices', ['ngResource']);
+
+loginServices.factory('OptionService', ['$resource',
+  function($resource){
+    return $resource('login/option', {}, {
+      query: {method:'GET', params:{optionid:'1'}, isArray:true}
+    });
+  }]);
--- routes/login.js
+++ routes/login.js
@@ -11,4 +11,14 @@ router.get('/',function(req,res){
     logger.debug("This is the Login module");
     res.render('loginpage/index',{title: 'Express'})
 });
+
+router.get('/partials/:option',function(req,res){
+    logger.debug("This is the Login module");
+    res.render('loginpage/partials/'+req.params.option,{title: 'Express'})
+});
+
+router.get('/option',function(req,res){
+    logger.debug("This is the Login module");
+    res.send("This responce came from option"+req.query.optionid)
+});
 module.exports = router;
